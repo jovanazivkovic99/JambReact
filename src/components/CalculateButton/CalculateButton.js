7516180bed6693e5e64952224eea3fb95984a0ef
Jovana Zivkovic <116586798+jovanazivkovic99@users.noreply.github.com>
2023-07-04 09:57:23 +0200

    made table smaller

--- src/components/CalculateButton/CalculateButton.js
+++ src/components/CalculateButton/CalculateButton.js
@@ -1,41 +1,61 @@
-import React, { useState } from "react"; // Don't forget to import useState
+import React, { useState, useEffect } from "react"; // Don't forget to import useState and useEffect
 import axios from "axios";
 import "./styles.css";
 
 const CalculateButton = (props) => {
-  const [sum, setSum] = useState(null); // Add this state variable
+  const [sum, setSum] = useState(0);
 
   const calculateSum = (template) => {
     let sum = 0;
-    // Logic to calculate the sum based on the template.numbers field.
+
     for (let row of template) {
-      for (let number of row) {
-        sum += Number(number); // assuming the numbers are strings
+      for (let cell of row) {
+        let numValue = parseFloat(cell.value);
+        if (cell.valid && !isNaN(Number(numValue))) {
+          sum += Number(cell.value);
+        }
       }
     }
+    console.log("Sum calculated:", sum);
     return sum;
   };
 
+  useEffect(() => {
+    console.log("Data received:", props.template);
+    let calculatedSum = calculateSum(props.template);
+    console.log("Initial sum:", calculatedSum);
+    setSum(calculatedSum);
+  }, [props.template]);
+
   const handleCalculateClick = async () => {
-    let template = [...props.template];
+    let calculatedSum = calculateSum(props.template);
 
-    let calculatedSum = calculateSum(template);
+    setSum(calculatedSum);
 
-    const templateWithSum = { numbers: template, sum: calculatedSum };
-
+    const templateWithSum = { numbers: props.template, sum: calculatedSum };
     const response = await axios.post("http://localhost:8081/template/result", templateWithSum);
 
     console.log("The total sum is:", response.data);
-
-    setSum(response.data.sum); // Update the state with the response sum
+    setSum(response.data.sum);
   };
+  /* const handleCalculateClick = async () => {
+    const templateWithSum = { numbers: props.template, sum: sum };
+    const response = await axios.post(
+      "http://localhost:8081/template/result",
+      templateWithSum
+    );
 
+    console.log("The total sum is:", response.data);
+    setSum(response.data.sum); // Update the state with the response sum
+  }; */
+  
   return (
     <div className="calculate-button-container">
       <button className="calculate-button" onClick={handleCalculateClick}>
         Calculate Sum
       </button>
-      {sum && <p className="sum-text">Total Sum: {sum}</p>} {/* Display the sum if it's not null */}
+      {sum !== null && <p className="sum-text">Total Sum: {sum}</p>}{" "}
+      {/* Display the sum if it's not null */}
     </div>
   );
 };
--- src/components/ParentComponent.js
+++ src/components/ParentComponent.js
@@ -10,40 +10,39 @@ const ParentComponent = () => {
   const [columns, setColumns] = useState([]);
   const [selectedFile, setSelectedFile] = useState(null);
 
+
   useEffect(() => {
     const fetchTableData = async () => {
       try {
         const formData = new FormData();
         formData.append("image", selectedFile);
 
-        const uploadResponse = await axios
-          .post("http://localhost:8081/template/save", formData)
-          .catch((error) => {
-            console.log("Error occurred while uploading image: ", error);
-          });
-
-        console.log("Upload Response:", uploadResponse); // Log upload response
+        const uploadResponse = await axios.post(
+          "http://localhost:8081/template/save",
+          formData
+        );
+        console.log("Upload Response:", uploadResponse);
 
         await new Promise((resolve) => setTimeout(resolve, 10000));
-        const imageId = uploadResponse.data;
 
-        console.log("Image ID:", imageId); // Log imageId
+        const imageId = uploadResponse.data;
+        console.log("Image ID:", imageId);
 
         const tableDataResponse = await axios.get(
           `http://localhost:8081/template/table/${imageId}`
         );
+        console.log("Table Data Response:", tableDataResponse);
 
-        console.log("Table Data Response:", tableDataResponse); // Log table data response
+        console.log("Table Data Response Data:", tableDataResponse.data);
 
-        const { rows } = tableDataResponse.data;
-
-        setTableData(rows);
-
-        if (rows.length > 0) {
-          const firstRow = rows[0];
-          const newColumns = Object.keys(firstRow).map((key) => ({
-            accessorKey: key,
-            header: key,
+        // Since your response is a 2D array, where each cell object contains both value and isValid,
+        // you should directly assign the response data to your tableData state variable.
+        setTableData(tableDataResponse.data);
+        if (tableDataResponse.data.length > 0) {
+          const firstRow = tableDataResponse.data[0];
+          const newColumns = firstRow.map((cell, index) => ({
+            accessorKey: index.toString(), // change this line
+            header: `Column ${index + 1}`,
           }));
           setColumns(newColumns);
         } else {
@@ -59,17 +58,31 @@ const ParentComponent = () => {
     }
   }, [selectedFile]);
 
+  useEffect(() => {
+    console.log("Log table data:", tableData);
+  }, [tableData]);
+
   const handleFileSelected = (file) => {
     setSelectedFile(file);
   };
- 
+
   return (
     <>
-    <UploadButton onFileSelected={handleFileSelected} />
-    <Box paddingX="5em" style={{ maxHeight: "100vh", overflowY: "auto" }}>
+      <UploadButton onFileSelected={handleFileSelected} />
+      <Box
+       display="flex"
+       justifyContent="center"
+       alignItems="center"
+       marginX="auto"
+       style={{
+         maxHeight: "100vh",
+         overflowY: "auto",
+         maxWidth: "100vh",
+       }}
+      >
         <MaterialReactTable
           columns={columns}
-          data={tableData}
+          data={tableData.map((row) => row.map((cell) => cell.value))} // convert each cell object into its value for the table
           enableColumnActions={false}
           enableColumnFilters={false}
           enablePagination={false}
@@ -79,33 +92,84 @@ const ParentComponent = () => {
           muiTableBodyRowProps={{ hover: false }}
           muiTableProps={{
             sx: {
-              maxWidth: "100%", // restrict table width to 100% of its container
-              maxHeight: "500px", // Adjust this value as needed
-              overflowX: "auto", // allow horizontal scrolling if the table's width exceeds its container's width
-              overflowY: "auto", // allow vertical scrolling if the table's height exceeds its container's height
+              maxWidth: "100%",
+              maxHeight: "400px",
+              height: "10px",
               border: "0.5px solid rgba(81, 81, 81, 0.5)",
             },
           }}
-          
           muiTableHeadCellProps={{
             sx: {
-                display: 'none'
+              display: "none",
             },
           }}
-          muiTableBodyCellProps={{
-            sx: {
-              minWidth: "30px", // Adjust as needed
-              maxWidth: "100px", // Adjust as needed
-              padding: "0.4em",
-              border: "0.5px solid rgba(81, 81, 81, 0.5)",
-              bgcolor:"#fcf5c7",
-              textAlign:"center"
-            },
+          muiTableBodyCellProps={(cellInfo) => {
+            const row = tableData[cellInfo.row.index];
+            const cell = row[cellInfo.column.id];
+            const cellIsValid = cell ? cell.valid : true;
+
+            return {
+              align: "center",
+              sx: {
+                minWidth: "50px",
+               // maxWidth: "100px",
+                padding: "0.4em",
+                border: "0.5px solid rgba(81, 81, 81, 0.5)",
+                bgcolor: cellIsValid ? "#ffffff" : "#fea49f",
+                textAlign: "center",
+                width:"0.1%",
+                whiteSpace: "nowrap",
+              },
+            };
+          }}
+          enableEditing
+          editingMode="table"
+          muiTableBodyCellEditTextFieldProps={({ cell }) => {
+            const commonProps = {
+              variant: "filled",
+              inputProps: {
+                style: { height: "25px", padding: "0px" },
+              },
+              InputProps: {
+                style: {
+                  height: "25px",
+                  padding: "10px"
+                },
+              },
+            };
+
+            if (parseInt(cell.column.id, 10) === 0) {
+              return {
+                ...commonProps,
+                disabled: true, // disable the TextField component
+              };
+            }
+
+            return {
+              ...commonProps,
+              onBlur: (event) => {
+                console.log("event: " + event + "cell: " + cell);
+                const updatedTableData = [...tableData];
+                updatedTableData[cell.row.index][cell.column.id].value =
+                  event.target.value;
+                setTableData(updatedTableData);
+                console.log(
+                  "table: " +
+                    tableData +
+                    "; updated table: " +
+                    updatedTableData +
+                    "; updated cell: " +
+                    updatedTableData[cell.row.index][cell.column.id].value +
+                    "; value to update: " +
+                    event.target.value
+                );
+              },
+            };
           }}
         />
       </Box>
+    
       <CalculateButton template={tableData} />
-
     </>
   );
 };
